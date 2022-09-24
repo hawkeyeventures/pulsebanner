308086ac12dd0ad7a10550838a1234d10b6d6fdc
Alex Weininger <alex.weininger@live.com>
2022-09-23 22:28:02 -0700

    Use resolve instead of lookup

--- apps/next/util/axios.ts
+++ apps/next/util/axios.ts
@@ -1,9 +1,43 @@
 import axios, { AxiosError } from "axios";
 import env from "./env";
 import { logger } from "./logger";
+import * as net from 'net';
+import * as dns from 'dns';
 
 export const twitchAxios = axios.create({
-    baseURL: 'https://api.twitch.tv'
+    baseURL: 'https://api.twitch.tv',
+});
+
+function resolve(hostname: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+        dns.resolve(hostname, (err, addresses) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(addresses[0]);
+            }
+        });
+    });
+}
+
+twitchAxios.interceptors.request.use(function (config) {
+    const url = new URL(config.url!);
+
+    if (net.isIP(url.hostname)) {
+        // Skip
+        return config;
+    } else {
+
+        return resolve(url.hostname).then((result) => {
+            config.headers = config.headers || {};
+            config.headers.Host = url.hostname; // put original hostname in Host header
+
+            url.hostname = result;
+            config.url = new URL(url).toString();
+
+            return config;
+        });
+    }
 });
 
 twitchAxios.interceptors.response.use(function (response) {
